a4608de09e73af2e995bf5a6e2754c9b324c8f98
Lesitsa <alesyagugaeva@mail.ru>
2022-03-27 21:28:44 +0300

    Работа с массивами

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,36 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    let dataArr = data.split(',');
+    dataArr.sort(function sortData(a, b) {
+        return a - b;
+    });
+    return dataArr.toString();
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    let result = data.filter(function (n) {
+        return n <= 100;
+    });
+    return result;
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let newArray = [];
+    for (let i = 0; i < array1.length; i++) {
+        newArray.push(array1[i]);
+        newArray.push(array2[i]);
+    }
+    return newArray;
+}
